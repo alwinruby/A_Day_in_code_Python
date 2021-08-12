bef30da836e04fbd9d8617660621118aca67b25f
alwinruby <alwinsolanky@gmail.com>
2021-08-12 21:56:39 +0100

    wake up program

---
+++ code/wake_up_program.py
@@ -0,0 +1,11 @@
+# Wake up Program
+clock_time = 9
+time_of_day = "AM"
+
+if clock_time == 8 and time_of_day == "AM":
+    alarm = "on"
+    print("Its 9AM! The alarm just turned " + alarm)
+    print("Wake up and stretch!")
+else:
+    alarm = "off"
+    print("Go back to sleep, the alarm is " + alarm)
